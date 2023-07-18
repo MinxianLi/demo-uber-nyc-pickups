2769a0eaa4609c658d5a50b7ce2e141a4bef2385
Mason Li <lmx423@gmail.com>
2023-07-18 13:25:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,194 +1,179 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018-2022 Streamlit Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""An example of showing geographic data."""
-
-import os
-
-import altair as alt
-import numpy as np
+from google.cloud import aiplatform
+import vertexai
+from vertexai.preview.language_models import TextGenerationModel
 import pandas as pd
-import pydeck as pdk
-import streamlit as st
+from enum import Enum
+vertexai.init(project="laxmanpersonal", location="us-central1")
+model = TextGenerationModel.from_pretrained("text-bison@001")
+def predict_large_language_model_sample(
+    temperature: float,
+    max_decode_steps: int,
+    top_p: float,
+    top_k: int,
+    content: str,
+    ) :
+    """Predict using a Large Language Model."""
+    response = model.predict(
+        content,
+        temperature=temperature,
+        max_output_tokens=max_decode_steps,
+        top_k=top_k,
+        top_p=top_p,)
+    # print(f"Response from Model: {response.text}")
+    return response
 
-# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
-st.set_page_config(layout="wide", page_title="NYC Ridesharing Demo", page_icon=":taxi:")
+prompt_template_0 = """Act as an expert care manager specialist supporting wellbeing and chronic conditions programs, you are talking to a member about their health conditions need to deliver the below message in quotes in an efficient personalized way to match their personal characteristics ,beliefs and socio economic conditions.
 
+"%s"
 
-# LOAD DATA ONCE
-@st.cache_resource
-def load_data():
-    path = "uber-raw-data-sep14.csv.gz"
-    if not os.path.isfile(path):
-        path = f"https://github.com/streamlit/demo-uber-nyc-pickups/raw/main/{path}"
+Rewrite the above message in quotes for a member in an efficient personalized way to match their personal characteristics , beliefs and socio economic conditions . Include relevant quotes and health content so it is for the member to understand and trust
+%s
+"""
+print(prompt_template_0)
+cc_text=[
+    "Being at a healthy weight can reduce your risk for diabetes, high blood pressure, stroke, and heart disease. Regular exercise and healthy eating can help you work toward and maintain a healthy weight. To get to a healthier weight, work with your health care provider to create a weight loss plan",
+    "Everyone should get the flu shot.The flu shot protects against both seasonal and H1N1 flu.The best time to get the flu shot is October through December.But you can still prevent the flu by getting a shot in January or February.Talk to your doctor about the flu shot.",
+    "Healthy eating can help you lower your risk for heart disease, high blood pressure, high cholesterol, and other health problems. You can start eating healthier by taking small steps. For instance, eat five or more servings of fruits and vegetables a day, limit the fatty foods and sweets you eat, and lower your salt intake. Talk to your health care provider to learn more about a healthy diet."
+]
 
-    data = pd.read_csv(
-        path,
-        nrows=100000,  # approx. 10% of data
-        names=[
-            "date/time",
-            "lat",
-            "lon",
-        ],  # specify names directly since they don't change
-        skiprows=1,  # don't read header since names specified directly
-        usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
-        parse_dates=[
-            "date/time"
-        ],  # set as datetime instead of converting after the fact
-    )
+cc_title=[
+    'CC_Lose_weight', 
+    'CC_Take_a_flushot', 
+    'CC_Eat_healthy'
+]
+class Age(Enum):
+    YOUNG='a young person'
+    OLD='a old person'
+    
+class Gender(Enum):
+    MALE='a male'
+    FEMALE='a female'
+    OTHER='other'
 
-    return data
+class Engagement(Enum):
+    LOW='Low level of engagement with health & wellness programs'
+    MEDIUM='Medium level of engagement with health & wellness programs'
+    HIGH='High level of engagement with health & wellness programs'
 
-
-# FUNCTION FOR AIRPORT MAPS
-def map(data, lat, lon, zoom):
-    st.write(
-        pdk.Deck(
-            map_style="mapbox://styles/mapbox/light-v9",
-            initial_view_state={
-                "latitude": lat,
-                "longitude": lon,
-                "zoom": zoom,
-                "pitch": 50,
-            },
-            layers=[
-                pdk.Layer(
-                    "HexagonLayer",
-                    data=data,
-                    get_position=["lon", "lat"],
-                    radius=100,
-                    elevation_scale=4,
-                    elevation_range=[0, 1000],
-                    pickable=True,
-                    extruded=True,
-                ),
-            ],
-        )
-    )
-
-
-# FILTER DATA FOR A SPECIFIC HOUR, CACHE
-@st.cache_data
-def filterdata(df, hour_selected):
-    return df[df["date/time"].dt.hour == hour_selected]
-
-
-# CALCULATE MIDPOINT FOR GIVEN SET OF DATA
-@st.cache_data
-def mpoint(lat, lon):
-    return (np.average(lat), np.average(lon))
-
-
-# FILTER DATA BY HOUR
-@st.cache_data
-def histdata(df, hr):
-    filtered = data[
-        (df["date/time"].dt.hour >= hr) & (df["date/time"].dt.hour < (hr + 1))
-    ]
-
-    hist = np.histogram(filtered["date/time"].dt.minute, bins=60, range=(0, 60))[0]
-
-    return pd.DataFrame({"minute": range(60), "pickups": hist})
+class Trust(Enum):
+    LOW_TRUST='Seek second opinions and multiple trusted, highly-recommended sources prior to making decisions'
+    HIGH_TRUST='Not looking for multiple opinions and multiple data points.'
+    EXPERT='Care about finding experts who understand and reliably support their specific health needs'
+    
+class HealthFoodAccess(Enum):
+    NO='No access to healthy food'
+    YES='Has access to healthy food'
+    
+class EconomicCondition(Enum):
+    POOR='Poor economic condition'
+    GOOD='Good economic condition'
+    
+class Education(Enum):
+    NO_DEGREE='Not have any degree'
+    HIGH_SCHOOL_DEGREE='Has a high school degree'
+    BACHELOR_DEGREE='Has a bachelor degree'
+    MASTER_DEGREE='Has a master degree'
+    DOCTORATE_DEGREE='Has a PhD degree'
 
 
-# STREAMLIT APP LAYOUT
-data = load_data()
-
-# LAYING OUT THE TOP SECTION OF THE APP
-row1_1, row1_2 = st.columns((2, 3))
-
-# SEE IF THERE'S A QUERY PARAM IN THE URL (e.g. ?pickup_hour=2)
-# THIS ALLOWS YOU TO PASS A STATEFUL URL TO SOMEONE WITH A SPECIFIC HOUR SELECTED,
-# E.G. https://share.streamlit.io/streamlit/demo-uber-nyc-pickups/main?pickup_hour=2
-if not st.session_state.get("url_synced", False):
-    try:
-        pickup_hour = int(st.experimental_get_query_params()["pickup_hour"][0])
-        st.session_state["pickup_hour"] = pickup_hour
-        st.session_state["url_synced"] = True
-    except KeyError:
-        pass
 
 
-# IF THE SLIDER CHANGES, UPDATE THE QUERY PARAM
-def update_query_params():
-    hour_selected = st.session_state["pickup_hour"]
-    st.experimental_set_query_params(pickup_hour=hour_selected)
-
-
-with row1_1:
-    st.title("NYC Uber Ridesharing Data")
-    hour_selected = st.slider(
-        "Select hour of pickup", 0, 23, key="pickup_hour", on_change=update_query_params
-    )
-
-
-with row1_2:
-    st.write(
-        """
-    ##
-    Examining how Uber pickups vary over time in New York City's and at its major regional airports.
-    By sliding the slider on the left you can view different slices of time and explore different transportation trends.
-    """
-    )
-
-# LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
-row2_1, row2_2, row2_3, row2_4 = st.columns((2, 1, 1, 1))
-
-# SETTING THE ZOOM LOCATIONS FOR THE AIRPORTS
-la_guardia = [40.7900, -73.8700]
-jfk = [40.6650, -73.7821]
-newark = [40.7090, -74.1805]
-zoom_level = 12
-midpoint = mpoint(data["lat"], data["lon"])
-
-with row2_1:
-    st.write(
-        f"""**All New York City from {hour_selected}:00 and {(hour_selected + 1) % 24}:00**"""
-    )
-    map(filterdata(data, hour_selected), midpoint[0], midpoint[1], 11)
-
-with row2_2:
-    st.write("**La Guardia Airport**")
-    map(filterdata(data, hour_selected), la_guardia[0], la_guardia[1], zoom_level)
-
-with row2_3:
-    st.write("**JFK Airport**")
-    map(filterdata(data, hour_selected), jfk[0], jfk[1], zoom_level)
-
-with row2_4:
-    st.write("**Newark Airport**")
-    map(filterdata(data, hour_selected), newark[0], newark[1], zoom_level)
-
-# CALCULATING DATA FOR THE HISTOGRAM
-chart_data = histdata(data, hour_selected)
-
-# LAYING OUT THE HISTOGRAM SECTION
-st.write(
-    f"""**Breakdown of rides per minute between {hour_selected}:00 and {(hour_selected + 1) % 24}:00**"""
-)
-
-st.altair_chart(
-    alt.Chart(chart_data)
-    .mark_area(
-        interpolate="step-after",
-    )
-    .encode(
-        x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
-        y=alt.Y("pickups:Q"),
-        tooltip=["minute", "pickups"],
-    )
-    .configure_mark(opacity=0.2, color="red"),
-    use_container_width=True,
-)
+    
+def getUserText(userattrs):
+    s=""
+    for attr in userattrs:
+        s+='- '+attr.value+'\n'
+    return s
+### Personalize by Age
+df = pd.DataFrame(columns = ['Member']+cc_title)
+users = [
+    [Age.YOUNG,Gender.MALE,Engagement.HIGH, Trust.EXPERT],
+    [Age.OLD,Gender.MALE,Engagement.HIGH, Trust.EXPERT]
+]
+for user in users:
+    
+    row = {
+        'Member' : user[0].value,
+        cc_title[0] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[0],getUserText(user))).text,
+        cc_title[1] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[1],getUserText(user))).text,
+        cc_title[2] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[2],getUserText(user))).text
+    }
+    df=df.append(row, ignore_index=True)
+# df
+print(prompt_template_0%(cc_text[0],getUserText(users[0])))
+pd.set_option('display.max_colwidth', None)
+df[['Member',cc_title[0]]]
+df[['Member',cc_title[1]]]
+df[['Member',cc_title[2]]]
+Personalize by Engagement
+df = pd.DataFrame(columns = ['Member']+cc_title)
+users = [
+    [Age.YOUNG,Gender.MALE,Engagement.HIGH, Trust.EXPERT],
+    [Age.YOUNG,Gender.MALE,Engagement.LOW, Trust.EXPERT]
+]
+for user in users:
+    
+    row = {
+        'Member' : user[2].value,
+        cc_title[0] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[0],getUserText(user))).text,
+        cc_title[1] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[1],getUserText(user))).text,
+        cc_title[2] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[2],getUserText(user))).text
+    }
+    df=df.append(row, ignore_index=True)
+df[['Member',cc_title[0]]]
+df[['Member',cc_title[1]]]
+df[['Member',cc_title[2]]]
+### Personalize by Health Food Access
+df = pd.DataFrame(columns = ['Member']+cc_title)
+users = [
+    [Age.YOUNG,Gender.MALE,Engagement.HIGH,Trust.EXPERT, HealthFoodAccess.NO],
+    [Age.YOUNG,Gender.MALE,Engagement.HIGH, Trust.EXPERT, HealthFoodAccess.YES]
+]
+for user in users:
+    
+    row = {
+        'Member' : user[2].value,
+        cc_title[0] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[0],getUserText(user))).text,
+        cc_title[1] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[1],getUserText(user))).text,
+        cc_title[2] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[2],getUserText(user))).text
+    }
+    df=df.append(row, ignore_index=True)
+df[['Member',cc_title[0]]]
+df[['Member',cc_title[1]]]
+df[['Member',cc_title[2]]]
+### Personalize by Economic Condition
+df = pd.DataFrame(columns = ['Member']+cc_title)
+users = [
+    [Age.YOUNG,Gender.MALE,Engagement.HIGH,Trust.EXPERT, HealthFoodAccess.YES, EconomicCondition.POOR],
+    [Age.YOUNG,Gender.MALE,Engagement.HIGH, Trust.EXPERT, HealthFoodAccess.YES, EconomicCondition.GOOD]
+]
+for user in users:
+    
+    row = {
+        'Member' : user[2].value,
+        cc_title[0] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[0],getUserText(user))).text,
+        cc_title[1] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[1],getUserText(user))).text,
+        cc_title[2] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[2],getUserText(user))).text
+    }
+    df=df.append(row, ignore_index=True)
+df[['Member',cc_title[0]]]
+df[['Member',cc_title[1]]]
+df[['Member',cc_title[2]]]
+### Personalize by Education
+df = pd.DataFrame(columns = ['Member']+cc_title)
+users = [
+    [Age.YOUNG,Gender.MALE,Engagement.HIGH,Trust.EXPERT, HealthFoodAccess.YES, EconomicCondition.GOOD, Education.NO_DEGREE],
+    [Age.YOUNG,Gender.MALE,Engagement.HIGH, Trust.EXPERT, HealthFoodAccess.YES, EconomicCondition.GOOD, Education.BACHELOR_DEGREE]
+]
+for user in users:
+    
+    row = {
+        'Member' : user[2].value,
+        cc_title[0] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[0],getUserText(user))).text,
+        cc_title[1] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[1],getUserText(user))).text,
+        cc_title[2] : predict_large_language_model_sample(0.2, 512, 0.8, 1, prompt_template_0%(cc_text[2],getUserText(user))).text
+    }
+    df=df.append(row, ignore_index=True)
+df[['Member',cc_title[0]]]
+df[['Member',cc_title[1]]]
+df[['Member',cc_title[2]]]
